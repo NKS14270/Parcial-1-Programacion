af2507ef3b68f3b03847565d0483cf7a1b4fb848
Nicolas <nicososa935@gmail.com>
2025-04-28 14:55:56 -0300

    Dbcontext

---
+++ Data/AppDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Parcial_1__Programacion.Models;
+
+namespace Parcial_1__Programacion.Data
+{
+        public class AppDBcontext : DbContext
+        {
+            public AppDBcontext(DbContextOptions<AppDBcontext> options) : base(options)
+            {
+            }
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer("Data Source=nico;Initial Catalog=PrimerParcial-Prog;Integrated Security=True;Trust Server Certificate=True");
+            }
+            public DbSet<Paciente> Pacientes { get; set; }
+            public DbSet<Obrasocial> Obrasociales { get; set; }
+
+    }
+}
